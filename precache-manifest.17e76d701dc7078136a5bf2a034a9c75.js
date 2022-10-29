b214b0bbe1472f3cf4055f9e23ccc8121f2095ba
Bipin <bipinsyadav9@gamil.com>
2022-10-29 21:37:53 +0530

    Updates

---
+++ precache-manifest.17e76d701dc7078136a5bf2a034a9c75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806aa17d11de657b8a02c5f3be02a9f6",
+    "url": "/Airbnb-clone/index.html"
+  },
+  {
+    "revision": "45941a76227103791304",
+    "url": "/Airbnb-clone/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "c59d3e77549303878c0b",
+    "url": "/Airbnb-clone/static/css/main.36115683.chunk.css"
+  },
+  {
+    "revision": "45941a76227103791304",
+    "url": "/Airbnb-clone/static/js/2.c7a0d04d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Airbnb-clone/static/js/2.c7a0d04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c59d3e77549303878c0b",
+    "url": "/Airbnb-clone/static/js/main.16572d48.chunk.js"
+  },
+  {
+    "revision": "2444f55e81115dd66e26",
+    "url": "/Airbnb-clone/static/js/runtime-main.fc129058.js"
+  }
+]);
